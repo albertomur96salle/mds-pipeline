e4857cc5d54410b25bbc74fdbc68aa61cc19dd32
Alberto Mur Rodrigo <alberto.mur@students.salle.url.edu>
2021-03-18 13:30:35 +0100

    Acceptance test for API service functionality added

---
+++ main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHelloWorld(t *testing.T) {
+	_, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatalf("could not created request: %v", err)
+	}
+	server := &Server{}
+	t.Run("return hello world message", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		request := createNewRequest()
+		server.ServeHTTP(response, request)
+
+		assertStatus(response.Code, http.StatusOK, t)
+		assertResponseBody(t, "{\"message\": \"hello world\"}", response.Body.String())
+
+	})
+}
+
+func createNewRequest() *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+	return request
+}
+
+func assertStatus(result int, expected int, t *testing.T) {
+	if result != expected {
+		t.Errorf("wrong status status result '%d', status expected '%d'", result, expected)
+	}
+}
+
+func assertResponseBody(t *testing.T, expected string, result string) {
+	if result != expected {
+		t.Errorf("result '%s', expected '%s'", expected, result)
+	}
+}
